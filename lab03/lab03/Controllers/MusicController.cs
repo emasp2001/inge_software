c93b36b6665a3c816f4f7b82a05bceae9912b04e
Emmanuel Solis <emmanuel.solisp@icloud.com>
2022-04-24 22:33:02 -0600

    new: lab 03 source code

---
+++ lab03/lab03/Controllers/MusicController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lab03.Models;
+using System;
+using System.Collections.Generic;
+
+namespace lab03.Controllers
+{
+  public class MusicController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
